7b1173a5a5c9a9a8ad3dfeeaee81ec7b5f576a9a
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2020-12-28 02:41:36 +0530

    Update read_robot_heading.php

--- earthrover/compass/control2/followup/read_robot_heading.php
+++ earthrover/compass/control2/followup/read_robot_heading.php
@@ -1,13 +1,8 @@
 <?php
 
-//echo"<h1>hihihihi</h1>";
-
-
 $myfile = fopen("../../robot_compass/heading.txt", "r") or die("Unable to open file!");
 $str = fread($myfile,5);
 fclose($myfile);
 
-
-//$str="29";
 echo "$str";
 ?>
